bc7428b27c516ea82838a01b991df81d3ceeb277
Milton Salles <milton.filho@ltmfidelidade.com.br>
2024-11-11 18:32:07 -0300

    fix: add log path chromium

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
 
+// Função para verificar o caminho do Chromium
+exec('which chromium', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Erro ao localizar o Chromium: ${error}`);
+    return;
+  }
+  console.log(`Chromium encontrado no caminho: ${stdout}`);
+});
+
 app.get('/get-client-id', async (req, res) => {
   try {
+    // Certifique-se de que o caminho do Chromium esteja correto
     const browser = await puppeteer.launch({
+      executablePath: '/usr/bin/chromium-browser', // Pode ser ajustado com o caminho correto
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true,
       defaultViewport: {
         width: 1280,
         height: 1024,
-      },
-      headless: true,
+      }
     });
     const page = await browser.newPage();
 
@@ -23,7 +35,7 @@ app.get('/get-client-id', async (req, res) => {
     });
 
     console.log('Aguardando o carregamento do gtag.js e a alteração do clientId...');
-    await page.waitForSelector('script[src*="gtag/js"]'); // Aguarda o carregamento do gtag.js
+    await page.waitForSelector('script[src*="gtag/js"]');
     await page.waitForFunction(
       'document.getElementById("clientId").innerText !== "Carregando..."',
       { timeout: 60000 }
@@ -40,7 +52,7 @@ app.get('/get-client-id', async (req, res) => {
     res.json({ clientId });
   } catch (error) {
     console.error('Erro ao obter o clientId:', error);
-    res.status(500).json({ error: `Erro ao obter o clientId: ${error.message}` });
+    res.status(500).json({ error: 'Erro ao obter o clientId' });
   }
 });
 
