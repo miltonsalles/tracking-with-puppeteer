246edec9e3b03012cb13711c82c781b4cb24c8d0
Milton Salles <milton.filho@ltmfidelidade.com.br>
2024-11-11 15:11:32 -0300

    fix: trackingId equal null

--- server.js
+++ server.js
@@ -8,33 +8,44 @@ async function getClientId(trackingId) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
-  await page.setContent(`
-    <html>
-      <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=${trackingId}"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${trackingId}');
-        </script>
-      </head>
-      <body>
-        <h1>Testando o gtag.js com ${trackingId}</h1>
-      </body>
-    </html>
-  `);
+  try {
+    await page.setContent(`
+      <html>
+        <head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=${trackingId}"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${trackingId}');
+          </script>
+        </head>
+        <body>
+          <h1>Testando o gtag.js com ${trackingId}</h1>
+        </body>
+      </html>
+    `);
 
-  const clientId = await page.evaluate(() => {
-    return new Promise((resolve) => {
-      gtag('get', trackingId, 'client_id', (clientId) => {
-        resolve(clientId);
+    // Espera o gtag.js carregar e captura o client_id do Google Analytics
+    const clientId = await page.evaluate((trackingId) => {
+      return new Promise((resolve, reject) => {
+        gtag('get', trackingId, 'client_id', (clientId) => {
+          if (clientId) {
+            resolve(clientId);
+          } else {
+            reject('Não foi possível obter o clientId');
+          }
+        });
       });
-    });
-  });
+    }, trackingId); 
 
-  await browser.close();
-  return clientId;
+    await browser.close();
+    return clientId;
+  } catch (error) {
+    console.error('Erro durante o processo do Puppeteer:', error);
+    await browser.close();
+    throw new Error('Erro ao obter o Client ID');
+  }
 }
 
 app.get('/clientId', async (req, res) => {
